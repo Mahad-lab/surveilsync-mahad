e1f307a0bf60b5b3f36734dd3994b8d7e0a9b5d8
Danyal Ali Asghar <danyal.aliasghar@gmail.com>
2025-08-03 18:02:06 +0500

    Danyal: First commit 03Aug

--- app/core/database.py
+++ app/core/database.py
@@ -3,6 +3,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # SQLITE format "sqlite:///./sqlite.db"
 # POSTGRES format "postgresql://user:password@postgresserver/db"
